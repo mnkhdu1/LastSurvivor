8949fc76650319993a6121fea4553faa86761ad2
EvilBastards23 <oneyepirate3@gmail.com>
2024-09-13 14:16:20 +0800

    world.alpha

--- scripts/enemy_poly.gd
+++ scripts/enemy_poly.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
+
+var speed = 1
 var player = null
-var speed = 0.1
 
 
 @onready var navigation_agent_3d = $NavigationAgent3D
@@ -17,6 +18,7 @@ func _process(delta):
 	navigation_agent_3d.set_target_position(player.global_transform.origin)
 	var next_navigation_point = navigation_agent_3d.get_next_path_position()
 	velocity = (next_navigation_point - global_transform.origin).normalized() * speed
+	print(velocity)
 	
 	look_at(Vector3(player.global_position.x,global_position.y,player.global_position.z),Vector3.UP)
 	move_and_slide()
